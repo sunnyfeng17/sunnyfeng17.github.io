1d889ad04143c9e209f8fb49a152a50c3c6309c0
Sunny Feng <sunnyfengnz@msn.com>
2020-04-16 00:00:25 +1200

    Updates

---
+++ precache-manifest.a6e202842c500fc5ed8ee95437e1de9f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23a5f45cb3f628aa40fe408a5e1de52e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a8b204b70652e43027f3",
+    "url": "/static/css/2.f3f0ba98.chunk.css"
+  },
+  {
+    "revision": "f311fa9c776ad00ad166",
+    "url": "/static/css/main.97f331ce.chunk.css"
+  },
+  {
+    "revision": "a8b204b70652e43027f3",
+    "url": "/static/js/2.7ea3ac26.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.7ea3ac26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f311fa9c776ad00ad166",
+    "url": "/static/js/main.9eedbe29.chunk.js"
+  },
+  {
+    "revision": "61bce0322354e9c642e7",
+    "url": "/static/js/runtime-main.380b4751.js"
+  },
+  {
+    "revision": "6e7861f937c1991e6d0bd688f78a9a14",
+    "url": "/static/media/continue.6e7861f9.svg"
+  },
+  {
+    "revision": "f0ddc407e9d3141f9c753b37def81ee0",
+    "url": "/static/media/home.f0ddc407.jpg"
+  },
+  {
+    "revision": "2e375c48c7cb53ed190fa7bea291cdf6",
+    "url": "/static/media/sunnyfeng.2e375c48.svg"
+  },
+  {
+    "revision": "cfeb720f6acaf3dd6da158a6e5aab543",
+    "url": "/static/media/sunnyfeng.cfeb720f.png"
+  }
+]);

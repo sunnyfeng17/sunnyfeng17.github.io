e75bc163b385a00b6ce4c0d0d8ea77ee80c9f796
Sunny Feng <sunnyfengnz@msn.com>
2020-08-27 17:18:08 +1200

    renamed _variables to _themes and made entire landing page a link

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -11,7 +11,8 @@ class LandingPage extends React.Component {
   render() {
     return (
       <div className="landing-container">
-        <div className="vertical-center landing-box">
+        <Link to="/home">
+          <div className="vertical-center landing-box">
             <Row>
               <Col span={24}>
                 <SunnySVG className="handwriting-effect" type="flex"/>
@@ -19,7 +20,8 @@ class LandingPage extends React.Component {
                 <Link to="/home"><ContinueSVG className="continue-button"/></Link>
               </Col>
             </Row>
-        </div>
+          </div>
+        </Link>
       </div>
     );
   }
